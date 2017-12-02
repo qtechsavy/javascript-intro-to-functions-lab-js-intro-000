4d55c0c91937ac3f97d7feee619a19fa3fbd335c
Gunu Shrestha <realgunu@gmail.com>
2017-12-02 00:28:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(hello){
+  return ('hello').toUpperCase('HELLO!')
+}
+function whisper(Hello){
+  return('Hello').toLowerCase('hello')
+}
+function logShout(HELLO){
+  console.log('HELLO')
+}
+function logWhisper(hello){
+  console.log('hello')
+}
+function sayHiToGrandma(hello){
+  return (`I can\'t hear you!`)
+}
+function sayHiToGrandma(hello){
+  return (`YES INDEED!`)
+}
